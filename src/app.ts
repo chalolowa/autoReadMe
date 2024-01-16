4c4883dbf2e272f579f2d5dec9c3e31a16a0a31c
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-01-16 22:59:32 +0300

    On branch master
     Changes to be committed:
    	modified:   .github/workflows/build.yml
    	new file:   build/app.js
    	new file:   build/fetchGithubData.js
    	modified:   src/app.ts
    	modified:   src/fetchGithubData.ts
    	deleted:    src/fetchRssData.ts

---
+++ src/app.ts
@@ -0,0 +1,80 @@
+import * as fs from "fs";
+const md = require("markdown-it")({
+  html: true, // Enable HTML tags in source
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  linkify: true // Autoconvert URL-like text to links
+});
+import { fetchGitHubData } from "./fetchGithubData";
+
+const ossProjectRepos = [
+  "node-v4-learn",
+  "EmailApplication",
+  "journal",
+  "FARMERSMARKET",
+  "elewa",
+  "autoReadMe"
+];
+
+const githubUsername = "Chalolowa";
+const websiteUrl = "https://fullstackdev.tech";
+const twitterUrl = "https://twitter.com/Locha_740";
+const linkedinUrl = "https://linkedin.com/in/wangwecharles";
+
+async function generateMarkdown() {
+  const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
+  const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
+  const twitterBadge = `[![Twitter Badge](https://img.shields.io/badge/-@iambolajiayo-3B7EBF?style=for-the-badge&logo=x&logoColor=white)](${twitterUrl})`;
+  const profileCountBadge = `![Profile Views Count Badge](https://komarev.com/ghpvc/?username=${githubUsername}&style=for-the-badge)`;
+
+  const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+  const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+
+  const markdownText = `<div align="center">\n
+
+  ${websiteBadge} ${linkedinBadge} ${twitterBadge} ${profileCountBadge}\n
+
+  ---\n
+
+  Hi there 👋🏾! I'm an innovative technology professional with progressive IT, web engineering, data, embedded systems, developer relations, documentation, technical writing, open-source, community building, and entrepreneurship experience in for-profit startups and non-profit technology and education organizations. I create technical content, build open-source projects and learning materials, speak/teach at developer meetups/conferences, and build several technical communities.\n
+
+  ---\n
+
+  ${githubStatsCardDark}\n
+  ${githubStatsCardLight}\n
+
+  </div>\n
+
+  ---\n
+
+  ## Highlights
+
+  <details>\n
+  <summary>OSS Projects</summary>\n
+  <br />
+  Here are some of my other projects you might want to check out that are not pinned:\n
+  <br />\n<br />
+  ${await fetchGitHubData(ossProjectRepos)}\n
+  </details>\n
+
+  <details>\n
+  <summary>Quick Tips</summary>\n\n
+  - 💬 How to reach me: DM [@iambolajiayo](https://twitter.com/iambolajiayo) on X (Twitter).\n
+  - 📖 Book recommendations: [Knowing God by J. I. Packer](https://bit.ly/3EdCFUW) and [Atomic Habits by James Clear](https://bit.ly/45r1kBH).\n
+  - 💙 Fun fact: I'm also a rugby player.\n
+  </details>\n
+
+  ---\n
+
+  <div align="center">\n
+   <a href="https://fullstackdev.tech" target="_blank" rel="noopener noreferrer"><img src="https://fullstackdev.tech/favicon.png" width="30" /></a>\n
+  </div>`;
+
+  const result = md.render(markdownText);
+
+  fs.writeFile("README.md", result, (error) => {
+    if (error) throw new Error(`Something went wrong: ${error}.`);
+    console.log(`✅ README.md file was succesfully generated.`);
+  });
+}
+
+generateMarkdown();
